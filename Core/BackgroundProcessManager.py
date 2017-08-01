a7407469c42a3d461626131dcdfd2de4fe44dbf5
stories2 <stories2@naver.com>
2017-08-01 15:33:30 +0900

    Background module : store user sent message

--- Core/BackgroundProcessManager.py
+++ Core/BackgroundProcessManager.py
@@ -1,11 +1,31 @@
 import json
+import threading
 from Utils import LoggingManager
 from Settings import DefineManager
 
+processingQueueDict = {}
+
 def UploadRawDatas(rawDataArray):
     LoggingManager.PrintLogMessage("Core", "UploadRawDatas", "parameter: " + str(rawDataArray), DefineManager.LOG_LEVEL_INFO)
-    return json.dumps({"Result": 1})
+
+    queueId = AddNewTrain(rawDataArray)
+
+    return json.dumps({"Result": queueId})
 
 def ForecastDatas(processId, day):
     LoggingManager.PrintLogMessage("Core", "UploadRawDatas", "parameter: id " + str(processId) + ", day " + str(day), DefineManager.LOG_LEVEL_INFO)
-    return json.dumps({"Status": "Done", "Result": []})
+
+    forecastedData = GetStoredTrain(processId, day)
+
+    return json.dumps({"Status": "Done", "Result": forecastedData})
+
+def AddNewTrain(rawDataArray):
+    nowDictSize = len(processingQueueDict)
+    processingQueueDict[nowDictSize] = rawDataArray
+    return nowDictSize
+
+def GetStoredTrain(processId, day):
+    if "Done" == DefineManager.ALGORITHM_STATUS_DONE:
+        return processingQueueDict[processId]
+    else:
+        return []
