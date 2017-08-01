69914705c4b82b537cfbcb2a82468e76c244e74f
stories2 <stories2@naver.com>
2017-08-01 11:20:44 +0900

    Define module : set up server address, port

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from Settings import DefineManager
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,5 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+	app.debug = True
+	app.run(host=DefineManager.SERVER_USING_HOST, port=DefineManager.SERVER_USING_PORT)
