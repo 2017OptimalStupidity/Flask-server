ae93c863178286d77a0572e0da1e403dddd02d67
stories2 <stories2@naver.com>
2017-08-08 13:43:50 +0900

    Firebase module : scenario tested

--- Core/BackgroundProcessManager.py
+++ Core/BackgroundProcessManager.py
@@ -2,9 +2,9 @@ import json
 import threading
 from Utils import LoggingManager
 from Settings import DefineManager
-from . import LearningManager
+from . import LearningManager, FirebaseDatabaseManager
 
-processingQueueDict = {}
+FirebaseDatabaseManager.GetFirebaseConnection(DefineManager.FIREBASE_DOMAIN)
 
 def UploadRawDatas(rawDataArray, day):
     LoggingManager.PrintLogMessage("BackgroundProcessManager", "UploadRawDatas", "parameter: " + str(rawDataArray) + ", day " + str(day), DefineManager.LOG_LEVEL_INFO)
@@ -16,13 +16,22 @@ def UploadRawDatas(rawDataArray, day):
 def ForecastDatas(processId):
     LoggingManager.PrintLogMessage("BackgroundProcessManager", "ForecastDatas", "parameter: id " + str(processId), DefineManager.LOG_LEVEL_INFO)
 
-    forecastedData = GetStoredTrain(processId)
+    forcastStatus, forecastedData = GetStoredTrain(processId)
 
-    return json.dumps({"Status": "Done", "Result": forecastedData})
+    status = "Working"
+
+    if forcastStatus == DefineManager.ALGORITHM_STATUS_DONE:
+        status = "Done"
+    else:
+        forecastedData = []
+
+    return json.dumps({"Status": status, "Result": forecastedData})
 
 def AddNewTrain(rawDataArray, day):
-    nowDictSize = len(processingQueueDict)
-    processingQueueDict[nowDictSize] = rawDataArray
+    nowDictSize = FirebaseDatabaseManager.GetLastProcessId() + 1
+    FirebaseDatabaseManager.CreateNewProcessTable(nowDictSize)
+    FirebaseDatabaseManager.StoreInputData(nowDictSize, rawDataArray, day)
+    FirebaseDatabaseManager.UpdateLastProcessId(nowDictSize)
 
     threadOfLearn = threading.Thread(target=LearningManager.LearningModuleRunner, args=(rawDataArray, nowDictSize, day))
     threadOfLearn.start()
@@ -31,11 +40,12 @@ def AddNewTrain(rawDataArray, day):
 
 def GetStoredTrain(processId):
 
-    processStatus = LearningManager.ProcessResultGetter(processId)[1]
-    processResult = LearningManager.ProcessResultGetter(processId)[0]
-
-    if processStatus == DefineManager.ALGORITHM_STATUS_DONE:
-        # LoggingManager.PrintLogMessage("Core", "GetStoredTrain", "dic: " + str(processingQueueDict[processId]), DefineManager.LOG_LEVEL_INFO)
-        return processResult
-    else:
-        return []
+    processStatus = FirebaseDatabaseManager.GetOutputDataStatus(processId)
+    processResult = FirebaseDatabaseManager.GetOutputDataArray(processId)
+    #
+    # if processStatus == DefineManager.ALGORITHM_STATUS_DONE:
+    #     # LoggingManager.PrintLogMessage("Core", "GetStoredTrain", "dic: " + str(processingQueueDict[processId]), DefineManager.LOG_LEVEL_INFO)
+    #     return processResult
+    # else:
+    #     return []
+    return processStatus, processResult
--- Core/LearningManager.py
+++ Core/LearningManager.py
@@ -2,7 +2,7 @@ from Settings import DefineManager
 from Utils import LoggingManager
 
 def LearningModuleRunner(rawArrayDatas, processId, day):
-    LoggingManager.PrintLogMessage("LearningManater", "LearningModuleRunner", "start of learning #" + processId, DefineManager.LOG_LEVEL_INFO)
+    LoggingManager.PrintLogMessage("LearningManater", "LearningModuleRunner", "start of learning #" + str(processId), DefineManager.LOG_LEVEL_INFO)
     return
 
 def ProcessResultGetter(processId):
