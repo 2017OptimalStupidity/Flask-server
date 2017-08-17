5fbd42c7eedb01bc4a41495093abdb0db6a1b09f
moonymit <hjmoon0710@naver.com>
2017-08-17 13:40:50 +0900

    코드정리

--- Core/LearningManager.py
+++ Core/LearningManager.py
@@ -18,39 +18,46 @@ def LearningModuleRunner(rawArrayDatas, processId, forecastDay):
     dataY = rawArrayDatas[1]
     testY = dataY[trainSize:]
     ############################################################### LSTM
-    # raw data를 preprocessed data로 변환하는 과정
-        # [[날짜],[판매량]] 형태로 되어있는 이차원 배열을 pandas.core.frame.DataFrame의 형태로 변환
-        # 1. ds추가가공 통해 요일, 계절로 변환(함수)
-        # 2. [년, 월, 요일, 판매량]
-        # 3. 추후) + 날씨, 경제 등 (함수)
+    # 전처리
+        # [[날짜],[판매량]] 형태 2D list -> pandas.core.frame.DataFrame의 형태의 [년, 월, 요일, 판매량]
+        #TODO ds를 요일, 계절로 변환을 PrepareLstm()에서 시행
+        #TODO 날씨, 경제 등의 feature 도 pandas DatFrame에 포함(PrepareLstm()에서)
 
+        #전체 data 전처리
     XY=PrepareLstm(rawArrayDatas)
-    X=XY[0][:trainSize]
+        #train data 추출(0.7)
+    X = XY[0][:trainSize]
     Y = XY[1][:trainSize]
     trainXY=[X,Y]
-    #trainXY는 XY에서 전체 행의 0.7 자른 것
 
     # 계산
+        #trainXY로 testY 예측
     mockForecast['Lstm'] = Lstm(preprocessedData=trainXY, forecastDay=testSize)
+        #전체 XY로 realForecast 예측
     realForecast['Lstm'] = Lstm(preprocessedData=XY, forecastDay=forecastDay)
 
-    #평가
+    # 평가
     testRmse=0
     for i in range(testSize):
-        testRmse=testRmse+ (testY[i] - mockForecast['Lstm'][i]) ** 2
+        testRmse=testRmse + (testY[i] - mockForecast['Lstm'][i]) ** 2
     rmse['Lstm'] = testRmse
 
     ################################################################ BAYSEIAN
 
-    #raw data를 preprocessed data(ds-y)로 변환하는 과정
-         #[[날짜],[판매량]] 형태로 되어있는 이차원 배열을 pandas.core.frame.DataFrame의 형태로 변환
+    # 전처리 raw data를 preprocessed data(ds-y)로 변환하는 과정
+        #[[날짜],[판매량]] 형태 2D list -> pandas.core.frame.DataFrame의 형태의 [날짜, 판매량]
+
+        # 전체 data 전처리
     XY = PrepareBayseian(rawArrayDatas)
-    X=XY[0][:trainSize]
+        # train data 추출(0.7)
+    X = XY[0][:trainSize]
     Y = XY[1][:trainSize]
     trainXY=[X,Y]
 
     #계산
+        # trainXY로 testY 예측
     mockForecast['Bayseian'] = Bayseian(preprocessedData=trainXY, forecastDay=testSize)[0]
+        # 전체 XY로 realForecast 예측
     realForecast['Bayseian'] = Bayseian(preprocessedData=XY, forecastDay=forecastDay)[0]
 
     #평가
@@ -59,20 +66,21 @@ def LearningModuleRunner(rawArrayDatas, processId, forecastDay):
         testRmse=testRmse+ (testY[i] - mockForecast['Bayseian'][i]) ** 2
     rmse['Bayseian'] = testRmse
 
-    ############################################################### 그 외 (ex SVM)
-    # 1. ds-y
+    ############################################################### 그 외 알고리즘(ex SVM)
+
 
 ######################################################################################################################
-    # 세개에서 나온 결과값을 다 취합해서 비교해서 가장 좋은 결과를 firebase에 저장
-    # 가장 좋은 결과의 평가기준은 예측값과 test set의 rmse
+    # numOfAlgorithmModules 개의 결과값(mockForecast)을 다 취합해서 비교 후 가장 좋은 결과를 firebase에 저장
+    # 가장 좋은 결과 평가기준: test set에 대한 <testY & mockForecast>의 rmse
     min = rmse['Lstm']
+    bestAlgorithmName='Lstm'
     for i in rmse.keys():
         if(min>rmse[i]):
             min=rmse[i]
             bestAlgorithmName=i
 
     realForecast=realForecast[bestAlgorithmName]
-    #processId = 0, resultArrayData = [], resultArrayDate = [], status = DefineManager.ALGORITHM_STATUS_WORKING)
+
     forecastDate= Bayseian(preprocessedData=XY, forecastDay=forecastDay)[1]
     FirebaseDatabaseManager.StoreOutputData(processId,forecastDate,realForecast,DefineManager.ALGORITHM_STATUS_DONE )
     return
@@ -96,7 +104,7 @@ def PrepareLstm(dsY):
     ds = dsY[0]
     # ds-->year, month, dayOfWeek 추출 #TODO
 
-    size=len(dsY)
+    size = len(dsY)
     year = np.random.beta(2000, 2017, size) * (2017 - 2000)
     month = np.random.beta(1, 12, size) * (12 - 1)
     dayOfWeek = np.random.beta(3, 6, size) * (6 - 0)#TODO: ValueError: a <= 0, KeyError: 0
@@ -105,6 +113,12 @@ def PrepareLstm(dsY):
     preprocessedData=[year, month, dayOfWeek, y]
     return preprocessedData
 
+def Lstm(preprocessedData,forecastDay):
+    forecast=[]
+    #일단은 random 출력
+    min=1 #min(preprocessedData[-1])
+    max=70 #max(preprocessedData[-1])
+    return list(np.random.beta(min, max, forecastDay)*(max-min))
 
 def PrepareBayseian(dsY):
     ds = dsY[0]
@@ -113,21 +127,15 @@ def PrepareBayseian(dsY):
     preprocessedData= pd.DataFrame(data = sales, columns=['ds', 'y'])
     return preprocessedData
 
-
-def Lstm(preprocessedData,forecastDay):
-    forecast=[]
-    #일단은 random 출력
-    min=1 #min(preprocessedData[-1])
-    max=70 #max(preprocessedData[-1])
-    return list(np.random.beta(min, max, forecastDay)*(max-min))
-
-def Bayseian(preprocessedData,forecastDay):
+def Bayseian(preprocessedData, forecastDay):
     forecast=[]
     model = Prophet()
     model.fit(preprocessedData)
     future = model.make_future_dataframe(periods=forecastDay)
+    forecast = future[-forecastDay:]
 
-    forecast=future[-forecastDay:]
-    temp = list(forecast['ds'][-forecastDay:])
-    date = [p.strftime('%Y-%m-%d') for p in temp]
+    #forecastDay에 해당하는 date형식을 출력
+    # (for firebase api: def StoreInputData(processId = 0, rawArrayData = [], rawArrayDate = [], day = 0))
+    dateStamp = list(forecast['ds'][-forecastDay:])
+    date = [p.strftime('%Y-%m-%d') for p in dateStamp]
     return forecast, date
