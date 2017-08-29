ddcb2b38f7ddd8d3f2cfcdc740b5523089340a7f
stories2 <stories2@naver.com>
2017-08-29 21:15:46 +0900

    Main module : disable debug, realse program

--- index.py
+++ index.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host=DefineManager.SERVER_USING_HOST, port=DefineManager.SERVER_USING_PORT)
+	# app.debug = True
+	app.run(host=DefineManager.SERVER_USING_HOST, port=DefineManager.SERVER_USING_PORT, threaded=True)
